82d19e210dddc6d6443b7885f0d10c7bbd341c99
t <t.brayko@innopolis.university>
2025-05-07 20:51:23 +0300

    Rollback to previous version of importing data

--- sql/import_data.sql
+++ sql/import_data.sql
@@ -1,9 +1,9 @@
 COPY stations FROM STDIN WITH CSV HEADER DELIMITER ',' NULL AS 'null';
 
--- COPY records (station_id, airnow_ozone, cmaq_ozone, cmaq_no2, cmaq_co, cmaq_oc, pressure, pbl, temperature, wind_speed, wind_direction, radiation, cloud_fraction, month, day, hour)
--- FROM STDIN WITH CSV HEADER DELIMITER ',' NULL AS 'null';
-
 COPY records
-FROM STDIN
-WITH CSV HEADER DELIMITER ','
-NULL AS 'null'
+    (station_id, airnow_ozone, cmaq_ozone, cmaq_no2, cmaq_co,
+     cmaq_oc, pressure, pbl, temperature, wind_speed,
+     wind_direction, radiation, cloud_fraction, month, day, hour)
+FROM STDIN WITH CSV HEADER
+DELIMITER ','
+NULL AS 'null';
