d22cf329fa9162b5e6e87ef58d2bbac01f143bec
1kkiren <1kkiren@mail.ru>
2025-05-07 06:21:58 +0300

    [add, update] output, stage2.sh

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -9,3 +9,10 @@ beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team29 -p $password 
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team29 -p $password -f sql/q4.hql
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team29 -p $password -f sql/q5.hql
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team29 -p $password -f sql/q6.hql
+
+hdfs dfs -cat project/output/q1/* >> output/q1.csv
+hdfs dfs -cat project/output/q2/* >> output/q2.csv
+hdfs dfs -cat project/output/q3/* >> output/q3.csv
+hdfs dfs -cat project/output/q4/* >> output/q4.csv
+hdfs dfs -cat project/output/q5/* >> output/q5.csv
+hdfs dfs -cat project/output/q6/* >> output/q6.csv
