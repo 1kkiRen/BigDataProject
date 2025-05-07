eea111f529ffd143f20627fded39983b398d1e1c
t <t.brayko@innopolis.university>
2025-05-07 22:59:28 +0300

    Check on existing parquest files on stage1.sh

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -8,6 +8,15 @@ if ! hdfs dfs -test -e project/warehouse ; then
   hdfs dfs -mkdir project/warehouse
 fi
 
+# Remove previous files
+if ! hdfs dfs -test -e project/warehouse/records ; then
+  hdfs dfs -rm -r project/warehouse/records
+fi
+
+if ! hdfs dfs -test -e project/warehouse/stations ; then
+  hdfs dfs -rm -r project/warehouse/stations
+fi
+
 password=$(head -n 1 secrets/.psql.pass)
 
 # Send data to HDFS via scoop
