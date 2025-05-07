8c818daf10f929bbb48babc262b697e79b89cc88
t <t.brayko@innopolis.university>
2025-05-07 18:47:53 +0300

    Add checks on files and dirs in  preprocess.sh and stage1.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -4,8 +4,27 @@
 if [ -z "$VIRTUAL_ENV" ]; then
   echo "Creating virtual environment"
   python3.6 -m venv .venv
+  source .venv/bin/activate
 fi
 
-source .venv/bin/activate
-python3.6 -m pip install --upgrade pip setuptools wheel
+python -m pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
+
+# Create 'data' directory
+if [ ! -d "data/" ]; then
+  mkdir data
+fi
+
+# Create 'secrets' directory
+if [ ! -f "secrets/" ]; then
+  mkdir secrets
+fi
+
+# Check all secret files with passwords are presented
+password_files=(".psql.pass" ".hive.pass")
+for file in $password_files; do
+  if [ ! -f "secrets/$file" ]; then
+    echo "WARN: not found $file in 'secrets' directory!
+     Please add secret file or enter password manually."
+  fi
+done
--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -10,13 +10,23 @@ fi
 # Launch stage 1
 python scripts/build_database.py
 
+
+# Create required directory in HDFS
+if ! hdfs dfs -test -e project/warehouse ; then
+  hdfs dfs -mkdir project/warehouse
+fi
+
+password=$(head -n 1 secrets/.psql.pass)
+
+#TODO: try --direct
 # Send data to HDFS via scoop
-#sqoop import-all-tables \
-#  --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team29_projectdb \
-#  --username team29 -P \
-#  --compression-codec=snappy \
-#  --compress --as-parquetfile \
-#  --warehouse-dir=project/warehouse \
-#  --m 1
+sqoop import-all-tables \
+  --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team29_projectdb \
+  --username team29 \
+  --password "$password" \
+  --compression-codec=snappy \
+  --compress --as-parquetfile \
+  --warehouse-dir=project/warehouse \
+  --m 1
 
 echo "Stage 1 done!"
